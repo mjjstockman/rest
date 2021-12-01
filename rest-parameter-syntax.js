7d3e4a0b61395d986cb8a4b0095ea7c6c5efd5c0
mjjstockman <mjjstockman@gmail.com>
2021-12-01 12:48:16 +0000

    Add notes on ...rest

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,22 @@
  */
 
 // Regular function call 
-
+const sumAll = (num1, num2, num3) => num1 + num2 + num3
 // Extra arguments are ignored
+console.log(sumAll(1, 1, 1, 5, 6, 7))
+
 
 // Function using ...rest
+// Extra arguments are captured in a var called rest.
+// rest can be accessed as any other var
+const sumAllRest = (num1, num2, num3, ...rest) => {
+    let sum = num1 + num2 + num3;
+    for (let num of rest) {
+        sum += num;
+    }
+    return sum;
+}
+
+
+let sum = sumAllRest(1, 2, 3, 4, 5, 6);
+console.log(sum);
